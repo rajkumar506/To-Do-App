085d413bdf5493d389fd3fd7dd413c92f510f661
rajkumar506 <rajkumarparmar506@gmail.com>
2020-10-09 22:03:19 +0530

    Updates

---
+++ precache-manifest.b281e6d299b200ef1e40bd236db11193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4adba557cdaac461c23046b8036974ab",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "aa1ad9843421c532d84a",
+    "url": "/To-Do-App/static/css/main.9e7f467d.chunk.css"
+  },
+  {
+    "revision": "2269e3afa499173fd057",
+    "url": "/To-Do-App/static/js/2.819cb7c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.819cb7c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1ad9843421c532d84a",
+    "url": "/To-Do-App/static/js/main.004b1016.chunk.js"
+  },
+  {
+    "revision": "8ca1dff9d7732f3d2c9b",
+    "url": "/To-Do-App/static/js/runtime-main.7beec4d7.js"
+  }
+]);
